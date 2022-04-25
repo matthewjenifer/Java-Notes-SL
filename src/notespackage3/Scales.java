a119fd1b90c99994800fdc0e49a5efaed67155d4
MATTHEW JENIFER <matthew36.j@gmail.com>
2022-04-25 17:51:58 -0400

    scale relations2

--- src/notespackage3/Scales.java
+++ src/notespackage3/Scales.java
@@ -14,7 +14,7 @@ public class Scales {
         // String[] aHarMinor = {"A","B","C","D","E","F","G#", "A"};
         // String[] aMajor = {"A","B","C#","D","E","F#","G#", "A"};
 
-        String rootNote = aAeoliMode[0]; // A is C Major's Natural Minor.         This means the 6th note on C Major's scale degree, is A Aeolian           (or A Nat)'s ROOT note. In other words, these scales are the same         - but with different centers of gravity (or "feeling") 
+        String rootNote = aAeoliMode[0]; // A is C Major's Natural Minor. This means the 6th note on C Major's scale degree, is A Aeolian (or A Nat)'s ROOT note. In other words, these scales are the same - but with different centers of gravity (or "feeling") 
         String sixthdegree = cMajor[5];
     
         if(rootNote == sixthdegree) {
