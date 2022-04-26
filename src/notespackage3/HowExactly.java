c83270e487a8c00345a2241fb5f40420d1893245
MATTHEW JENIFER <matthew36.j@gmail.com>
2022-04-26 00:51:02 -0400

    how exactly

--- src/notespackage3/HowExactly.java
+++ src/notespackage3/HowExactly.java
@@ -3,12 +3,15 @@ package notespackage3;
 public class HowExactly {
     public static void main(String[] args) {
         int sum = 0;
+        System.out.println(sum);
         for(int i = 1; i <= 5; i++) {
+        System.out.println(i);
             if(i == 4) {
+        System.out.println(i);
                 continue;
             }
             sum += i;
         }
-        System.out.println(sum); // prints: 11???
+        System.out.println(sum); // prints: 11??? 
     }
 }
