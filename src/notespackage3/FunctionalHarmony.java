5ff06d829c57779f9929f4d15dd75990b1281a5a
MATTHEW JENIFER <matthew36.j@gmail.com>
2022-04-28 03:23:34 -0400

    more on harmony

--- src/notespackage3/FunctionalHarmony.java
+++ src/notespackage3/FunctionalHarmony.java
@@ -3,8 +3,8 @@ package notespackage3;
 public class FunctionalHarmony {
     public static void main(String[] args) {
         String[][] progression = {
-            {"C","Dm","Em","F","G","Am","Bo"},
-            {"I","ii","iii","IV","V","vi","vii"}
+            {"C","Dm","Em","F","G","Am","Bo", "D","E"},
+            {"I","ii","iii","IV","V","vi","vii","V/V","V/vi"}
         };
         String cMaj = progression[0][0];
         String dMin = progression[0][1];
@@ -13,8 +13,8 @@ public class FunctionalHarmony {
         String gMaj = progression[0][4];
         String aMin = progression[0][5];
         String bDim = progression[0][6];
-        String dMaj = "D";
-        String eMaj = "E";
+        String dMaj = progression[0][7];
+        String eMaj = progression[0][8];
 
         String chord1 = progression[1][0];
         String chord2 = progression[1][1];
@@ -23,21 +23,21 @@ public class FunctionalHarmony {
         String chord5 = progression[1][4];
         String chord6 = progression[1][5];
         String chord7 = progression[1][6];
-        String chord8 = "V/V";
-        String chord9 = "V/vi";
+        String chord8 = progression[1][7];
+        String chord9 = progression[1][8];
 
         System.out.println(" ");
         System.out.println("----------------");
         System.out.println("(C Progression)");
         System.out.println("--------------");
-        System.out.println("Chord: " + cMaj +" (" + chord1 + ")");
-        System.out.println("Chord: " + dMin +" (" + chord2 + ")");
-        System.out.println("Chord: " + eMin +" (" + chord3 + ")");
-        System.out.println("Chord: " + fMaj +" (" + chord4 + ")");
-        System.out.println("Chord: " + gMaj +" (" + chord5 + ")");
-        System.out.println("Chord: " + aMin +" (" + chord6 + ")");
-        System.out.println("Chord: " + bDim +" (" + chord7 + ")");
-        System.out.println("Chord: " + cMaj +" (" + chord1 + ")");
+        System.out.println("T - Chord: " + cMaj +" (" + chord1 + ")");
+        System.out.println("SD- Chord: " + dMin +" (" + chord2 + ")");
+        System.out.println("T - Chord: " + eMin +" (" + chord3 + ")");
+        System.out.println("SD- Chord: " + fMaj +" (" + chord4 + ")");
+        System.out.println("D - Chord: " + gMaj +" (" + chord5 + ")");
+        System.out.println("SD- Chord: " + aMin +" (" + chord6 + ")");
+        System.out.println("D - Chord: " + bDim +" (" + chord7 + ")");
+        System.out.println("T - Chord: " + cMaj +" (" + chord1 + ")");
         System.out.println(" ");
         System.out.println("-----------------");
         System.out.println("(TONIC Chords a.k.a \"Home\" or \"Root\")");
@@ -47,7 +47,7 @@ public class FunctionalHarmony {
         System.out.println(" ");
         System.out.println("----from TONIC we can go SUB or DOM..");
         System.out.println("----------------");
-        System.out.println("(SUB-DOMINANT Chords)");
+        System.out.println("(SUB or PRE-DOMINANT Chords)");
         System.out.println(" ");
         System.out.println("SD- (" + chord2 + ") " + dMin);
         System.out.println("SD- (" + chord4 + ") " + fMaj);
@@ -64,21 +64,25 @@ public class FunctionalHarmony {
         System.out.println(" ");
         System.out.println("(SECONDARY Dominant Chords)");
         System.out.println(" ");
-        System.out.println("--think of 2ndDom chords like \"5 of 5\"(V/V)..");
-        System.out.println("--or if G is 5deg from C, & D is..");
-        System.out.println("--5deg from G: then D is the 2ndDOM of C..");
+        System.out.println("--think of 2ndDom chords like \"5 of 5\"(V/V)");
         System.out.println(" ");
+        System.out.println("--or if G is 5deg from C, & D is 5deg from G:");
+        System.out.println("then D is the 2ndDOM of C..");
+        System.out.println(" ");
+        System.out.println("----------");
         System.out.println("Chord: " + cMaj +" (" + chord1 + ")");
         System.out.println("Chord: " + fMaj +" (" + chord4 + ")");
-        System.out.println("**2ndDOM: " + dMaj +" (" + chord8 + ")**");
+        System.out.println("Chord: " + dMaj +" (" + chord8 + ") <--2nd Dominant");
         System.out.println("Chord: " + gMaj +" (" + chord5 + ")");
         System.out.println("Chord: " + cMaj +" (" + chord1 + ")");
+        System.out.println("----------");
         System.out.println(" ");
-        System.out.println("--OR: if Amin is 6deg from C, & E is..");
-        System.out.println("--5deg from Amin(V/vi): then E becomes 2ndDOM of C..");
+        System.out.println("--OR: if Amin is 6deg from C, & E is 5deg from Amin:");
+        System.out.println("then E(V/vi) becomes 2ndDOM of C..");
         System.out.println(" ");
+        System.out.println("----------");
         System.out.println("Chord: " + cMaj +" (" + chord1 + ")");
-        System.out.println("**2ndDOM: " + eMaj +" (" + chord9 + ")**");
+        System.out.println("Chord: " + eMaj +" (" + chord9 + ") <--2nd Dominant");
         System.out.println("Chord: " + aMin +" (" + chord6 + ")");
         System.out.println("Chord: " + gMaj +" (" + chord5 + ")");
         System.out.println("Chord: " + cMaj +" (" + chord1 + ")");
