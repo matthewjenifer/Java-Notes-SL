8efb574e689f8444375b45db71f88db31c76a8a6
MATTHEW JENIFER <matthew36.j@gmail.com>
2022-04-27 20:16:51 -0400

    fingering added

--- src/notespackage3/DAngelo.java
+++ src/notespackage3/DAngelo.java
@@ -7,23 +7,35 @@ public class DAngelo { // https://www.youtube.com/watch?v=EHihOgIe3U4
         String mode = "Phrygian";
         System.out.println("Another Life by DAngelo");
         System.out.println("Key:" + key + " | Mode: "+ mode);
-        String[][] anotherlife = {{"A#mi7","Bmaj7","F#maj7","G#m11"},{"7","6","8","8"}};
+        String[][] anotherlife = {
+            {"A#mi7","Bmaj7","F#maj7","G#m11"},
+            {"7","6","8","8"},
+            {"F#","F","G","G"}
+        };
         String i = anotherlife[0][0];
         String II = anotherlife[0][1];
         String IV = anotherlife[0][2];
         String vi = anotherlife[0][3];
+
         String mk3a = anotherlife[1][0];
         String mk3b = anotherlife[1][1];
         String mk3c = anotherlife[1][2];
         String mk3d = anotherlife[1][3];
+
+        String finger1 = anotherlife[2][0];
+        String finger2 = anotherlife[2][1];
+        String finger3 = anotherlife[2][2];
+        String finger4 = anotherlife[2][3];
+
         String root1 = "Dmi";
         String root2 = "D#mi";
         String root3 = "Bmi9";
+        
         System.out.println("-----------------");
-        System.out.println("Min4 [r]("+ root1 +"): " + " Pad-" + mk3a + " (" + i + ")" );
-        System.out.println("Min4 [r]("+ root2 +"): " + " Pad-" + mk3b + " (" + II + ")" );
-        System.out.println("Min4 [r]("+ root2 +"): " + " Pad-" + mk3c+ " (" + IV + ")" );
-        System.out.println("Min7 [r]("+ root3 +"): "+ " Pad-" + mk3d + " (" + vi + ")" );
+        System.out.println("Min4 [r]("+ root1 +"): " + " Pad-" + mk3a + " (" + i  + ")" + " _Hand: " + finger1 );
+        System.out.println("Min4 [r]("+ root2 +"): " + " Pad-" + mk3b + " (" + II + ")" + " _Hand: " + finger2 );
+        System.out.println("Min4 [r]("+ root2 +"): " + " Pad-" + mk3c + " (" + IV + ")" + " _Hand: " + finger3 );
+        System.out.println("Min7 [r]("+ root3 +"): " + " Pad-" + mk3d + " (" + vi + ")" + " _Hand: " + finger4 );
         System.out.println("-----------------");
     }
 }
--- src/notespackage3/KateBush.java
+++ src/notespackage3/KateBush.java
@@ -6,7 +6,8 @@ public class KateBush { // https://www.hooktheory.com/theorytab/view/kate-bush/b
         System.out.println("Babooshka by Kate Bush");
         String[][] babooshka = {
             {"G#mi","C#", "Eb","Ebm","C#sus24","B","Bmaj7","Bbm7","G#m7"},
-            {"6","4","5","6","8","5","6","7","7"}
+            {"6","4","5","6","8","5","6","7","12"},
+            {"F","Eb","E","F","G","E","F","F#","B"}
         };
         String iv = babooshka[0][0];
         String VII6 = babooshka[0][1];
@@ -15,8 +16,9 @@ public class KateBush { // https://www.hooktheory.com/theorytab/view/kate-bush/b
         String VIIsus2sus4 = babooshka[0][4];
         String VI = babooshka[0][5];
         String VI7 = babooshka[0][6];
-        String vi7 = babooshka[0][7];
+        String V7 = babooshka[0][7];
         String iv7 = babooshka[0][8];
+
         String mk3a = babooshka[1][0];
         String mk3b = babooshka[1][1];
         String mk3c = babooshka[1][2];
@@ -26,20 +28,43 @@ public class KateBush { // https://www.hooktheory.com/theorytab/view/kate-bush/b
         String mk3g = babooshka[1][6];
         String mk3h = babooshka[1][7];
         String mk3i = babooshka[1][8];
+
+        String finger1 = babooshka[2][0];
+        String finger2 = babooshka[2][1];
+        String finger3 = babooshka[2][2];
+        String finger4 = babooshka[2][3];
+        String finger5 = babooshka[2][4];
+        String finger6 = babooshka[2][5];
+        String finger7 = babooshka[2][6];
+        String finger8 = babooshka[2][7];
+        String finger9 = babooshka[2][8];
+
         String root1 = "D#mi";
         String root2 = "Bbmi";
 
-        String root3 = "F#mi";
+        String root3 = "G#mi";
         String root4 = "";
+
         System.out.println("-----------------");
+        System.out.println("(Intro - x4)");
         System.out.println("Min3 [r]("+ root1 +"): " + " Pad-" + mk3a + " (" + iv + ")" );
         System.out.println("Min3 [r]("+ root1 +"): " + " Pad-" + mk3b + " (" + VII6 + ")" );
         System.out.println("Min3 [r]("+ root2 +"): " + " Pad-" + mk3c+ " (" + I6 + ")" );
-
+        System.out.println("-----------------");
+        System.out.println("(Verse)");
         System.out.println("Min4 [r]("+ root2 +"): "+ " Pad-" + mk3d + " (" + i + ")" );
         System.out.println("Min4 [r]("+ root3 +"): "+ " Pad-" + mk3e + " (" + VIIsus2sus4 + ")" );
         System.out.println("Min4 [r]("+ root3 +"): " + " Pad-" + mk3f + " (" + VI + ")" );
+        System.out.println("Min3 [r]("+ root2 +"): " + " Pad-" + mk3c+ " (" + I6 + ")" );
         System.out.println("-----------------");
+        System.out.println("Min3 [r]("+ root2 +"): " + " Pad-" + mk3c+ " (" + VI7 + ")" );
+        System.out.println("Min3 [r]("+ root2 +"): " + " Pad-" + mk3c+ " (" + V7 + ")" );
+        System.out.println("Min3 [r]("+ root2 +"): " + " Pad-" + mk3c+ " (" + iv7 + ")" );
+        System.out.println("Min3 [r]("+ root1 +"): " + " Pad-" + mk3b + " (" + VII6 + ")" );
+        System.out.println("Min3 [r]("+ root2 +"): " + " Pad-" + mk3c+ " (" + I6 + ")" );
+        // System.out.println("Min4 [r]("+ root2 +"): "+ " Pad-" + mk3d + " (" + i + ")" );
+        // System.out.println("Min4 [r]("+ root3 +"): "+ " Pad-" + mk3e + " (" + VIIsus2sus4 + ")" );
+        // System.out.println("Min4 [r]("+ root3 +"): " + " Pad-" + mk3f + " (" + VI + ")" );
     }
 }
 
